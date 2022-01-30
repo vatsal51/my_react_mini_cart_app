9337ff7881ecda4a85bf33c08f6a2c8e6c3096c3
VATSAL PATEL <vatsal539241@gmail.com>
2022-01-31 01:38:51 +0530

    fetching data & implementing funtionalies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import CartPage from './CartPage';
+import React, { useState } from "react";
+import MiniCart from "./MiniCart";
+import cart from "./delivery-cart-svgrepo-com.svg";
+import product2 from "./ad-product-svgrepo-com.svg";
+import "./App.css";
+export default function App() {
+  const [products, setProducts] = useState([]);
+  const [visible, setVisible] = useState(false);
 
-function App() {
-  const [items,setItems] = useState([]);
-
-  useEffect(()=>{
+  React.useEffect(() => {
     fetch("https://dnc0cmt2n557n.cloudfront.net/products.json")
-    .then(response => response.json())
-    .then((data)=>
-     setItems(data)
-     
-    )
-},[]);
+      .then((res) => res.json())
+      .then((json) => {
+        let temp = [];
+        json.products.forEach((element) => {
+          element["quantity"] = 1;
+          temp.push(element);
+        });
+        console.log(temp);
+        setProducts(temp);
+      });
+  }, []);
 
+  const decrementCounter = (user) => {
+    const UpdateProduct = products.map((prod) =>
+      prod.id === user.id
+        ? { ...prod, quantity: prod.quantity === 0 ? 0 : prod.quantity - 1 }
+        : prod
+    );
+    setProducts(UpdateProduct);
+  };
+
+  const incrementCounter = (user) => {
+    const UpdateProduct = products.map((prod) =>
+      prod.id === user.id ? { ...prod, quantity: prod.quantity + 1 } : prod
+    );
+    setProducts(UpdateProduct);
+  };
+
+  const ToggleCart = () => {
+    setVisible(!visible);
+    console.log("clicked", { visible });
+  };
+
+  let cost = 0;
+  products.map((e) => {
+    cost += e.price * e.quantity;
+  });
+
+  let item = 0;
+  products.map((e) => {
+    if (e.quantity > 0) {
+      item += 1;
+    }
+  });
+
+  let curr = "";
+  products.map((e) => {
+    if (e.currency) {
+      curr = e.currency;
+    }
+  });
   return (
-    <div className="App box">
-    
-     <CartPage item={items} />
-    </div>
-  
-  )
-}
+    <div className="ProductContainer">
+      <h1>Products</h1>
+      <div className="cart-header">
+        <span style={{fontWeight:"bold"}}> {curr}{cost} </span>
+        <span>{item}&nbsp; items</span>
+        <img className="svg-size" src={cart} alt="cart" onClick={ToggleCart} ></img>
+      </div>
 
-export default App;
+      {visible ? (
+        <MiniCart
+          products={products}
+          setProducts={setProducts}
+          cost={cost}
+          item={item}
+        />
+      ) : (
+        ""
+      )}
+
+
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <span>
+                <img
+                  src={product2}
+                  lt="product"
+                  className="svg-size"
+                  alt="product"
+                ></img>
+              </span>
+              <span>{product.title}</span>
+              <span>{product.desc}</span>
+              <button onClick={() => decrementCounter(product)}> - </button>
+              <input
+                className="inp-box"
+                type="text"
+                value={product.quantity}
+              ></input>
+              <button onClick={() => incrementCounter(product)}> + </button>
+              <span>
+                {product.currency}
+                {product.price}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/MiniCart.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Cross from "./remove-svgrepo-com.svg"
+function MiniCart({ setProducts, products, item, cost }) {
+  const deleteProduct = (id) => {
+    const UpdateProduct = products.map((prod) =>
+      prod.id === id ? { ...prod, quantity: 0 } : prod
+    );
+    setProducts(UpdateProduct);
+  };
+
+  return (
+    <div className="miniCart">
+      {item > 0
+        ? products.map((product) => {
+            if (product.quantity > 0) {
+              return (
+                 
+                <div key={product.id} className="miniCart_products_wrapper">
+                  <div className="miniCart_products">
+                    <img src={Cross} className="svg-cross" onClick={() => deleteProduct(product.id)}></img>
+                  {/* <button onClick={() => deleteProduct(product.id)}>x</button> */}
+                    <span>{product.title}</span>   
+                  <span style={{fontWeight:"bold"}}>{product.currency} {product.price}</span>
+                  <span className="quantity">Qty:{product.quantity}</span>
+                  </div>
+                </div>
+              );
+            }
+          })
+        : "NO ITEMS IN CART"}
+
+    
+    </div>
+  );
+}
+
+export default MiniCart;
